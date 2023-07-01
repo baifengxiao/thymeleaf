8db54bf478e396c12e4523ac3e9290e952cd70b7
yupengtao <javaer@111.com>
2023-07-01 15:38:32 +0800

    重构4：thymeleaf逻辑视图配置

--- src/main/java/cn/sc/love/thymeleaf/controller/IndexServlet.java
+++ src/main/java/cn/sc/love/thymeleaf/controller/IndexServlet.java
@@ -1,11 +1,11 @@
 package cn.sc.love.thymeleaf.controller;
 
 import cn.sc.love.thymeleaf.fruit.dao.impl.FruitDAOImpl;
+import cn.sc.love.thymeleaf.myssm.myspringmvc.ViewBaseServlet;
 import cn.sc.love.thymeleaf.pojo.Fruit;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,13 +17,16 @@ import java.util.List;
  * @Date 2023/7/1 12:22
  **/
 @WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+public class IndexServlet extends ViewBaseServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         FruitDAOImpl fruitDAO = new FruitDAOImpl();
         List<Fruit> fruitList = fruitDAO.getFruitList();
         HttpSession session = req.getSession();
-        session.setAttribute("list",fruitList);
+        session.setAttribute("list", fruitList);
+
+//        逻辑视图名称"index",物理视图名称在此基础上加上前缀后缀
+        super.processTemplate("index", req, resp);
     }
 }

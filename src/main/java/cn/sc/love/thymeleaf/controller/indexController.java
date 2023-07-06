c22be68c27efd33d905e990933658eb046b79107
yupengtao <javaer@111.com>
2023-07-06 21:47:37 +0800

    提取出单thymeleaf项目，Javaweb其他内容转移到其他项目中

--- src/main/java/cn/sc/love/thymeleaf/controller/indexController.java
+++ src/main/java/cn/sc/love/thymeleaf/controller/indexController.java
@@ -1,21 +1,27 @@
 package cn.sc.love.thymeleaf.controller;
 
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author YPT
  * @create 2023-05-16-21:43
  */
 
-//@Controller
-//public class indexController {
-//
-//    @GetMapping("index")
-//    public String index(HttpServletRequest request){
-//
-//        request.setAttribute("name","柳岩");
-//
-//        List<String> list = Arrays.asList("刘德华", "张学友", "周星驰","刘一菲","李伟");
-//        request.setAttribute("list",list);
-//
-//        return "index";
-//    }
-//}
+@Controller
+public class indexController {
+
+    @GetMapping("index")
+    public String index(HttpServletRequest request){
+
+        request.setAttribute("name","柳岩");
+
+        List<String> list = Arrays.asList("刘德华", "张学友", "周星驰","刘一菲","李伟");
+        request.setAttribute("list",list);
+
+        return "index";
+    }
+}

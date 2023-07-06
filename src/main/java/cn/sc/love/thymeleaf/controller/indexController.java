c8f2ab4a244c0aae0caafa119e8a1481db3b5bb3
yupengtao <javaer@111.com>
2023-07-06 21:55:25 +0800

    提取出单thymeleaf项目，Javaweb其他内容转移到其他项目中

--- src/main/java/cn/sc/love/thymeleaf/controller/indexController.java
+++ src/main/java/cn/sc/love/thymeleaf/controller/indexController.java
@@ -1,9 +1,8 @@
 package cn.sc.love.thymeleaf.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -21,7 +20,7 @@ import java.util.Map;
 public class indexController {
 
     @GetMapping("index")
-    public String index(HttpServletRequest request, HttpSession session, Model model,HashMap hashMap) {
+    public String index(HttpServletRequest request, HttpSession session, Model model, HashMap hashMap) {
 
         request.setAttribute("name", "柳岩");
 

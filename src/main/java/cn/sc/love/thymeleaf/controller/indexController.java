9de48e1455d82ff88a7b9e1b9fddfdef5505f678
yupengtao <javaer@111.com>
2023-07-06 21:57:35 +0800

    提取出单thymeleaf项目，Javaweb其他内容转移到其他项目中

--- src/main/java/cn/sc/love/thymeleaf/controller/indexController.java
+++ src/main/java/cn/sc/love/thymeleaf/controller/indexController.java
@@ -3,6 +3,8 @@ package cn.sc.love.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
